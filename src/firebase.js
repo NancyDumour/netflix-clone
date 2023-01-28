25f04124fe952297419157820ddba9f155e88c06
Nancy Dumour <nancydumour96@gmail.com>
2023-01-28 21:07:33 +0200

    Authentication with Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey:
+    process.env.FIREBASE_API_KEY || "AIzaSyBIhxL52GlMrZGGPZvRqN45tKJVnViQT-U",
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
@@ -12,3 +14,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
