2332cb6f0100c3eb810733dbe3dac6a32d8f06ba
Nancy Dumour <nancydumour96@gmail.com>
2023-01-28 18:49:21 +0200

    Creating Main Component and Adding API Calls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 };
---
+++ src/components/Main.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import requests from "../Requests";
+
+const Main = () => {
+  const [movies, setMovies] = useState([]);
+
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
+
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
+
+  return (
+    <div className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black" />
+        <img
+          className="w-full h-full object-cover"
+          alt={movie?.title}
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+        />
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <div className="my-4">
+            <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
+              Play
+            </button>
+            <button className="border btext-white border-gray-300 py-2 px-5 ml-4">
+              Watch Later
+            </button>
+          </div>
+          <p className="text-gray-400 text-sm">
+            Released: {movie?.release_date}
+          </p>
+          <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+            {truncateString(movie?.overview, 150)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
