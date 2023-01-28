4e26d071594ac895d9ded590e4673192f8734fb1
Nancy Dumour <nancydumour96@gmail.com>
2023-01-28 22:17:09 +0200

    Removing keys:

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,11 @@ import { initializeFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey:
     process.env.FIREBASE_API_KEY || "AIzaSyBIhxL52GlMrZGGPZvRqN45tKJVnViQT-U",
-  authDomain:
-    process.env.FIREBASE_AUTH_DOMAIN || "netflix-clone-c16e2.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROJECT_ID || "netflix-clone-c16e2",
-  storageBucket:
-    process.env.FIREBASE_STORAGE_BUCKET || "netflix-clone-c16e2.appspot.com",
-  messagingSenderId: process.env.MESSAGING_SENDER_ID || "1098482235546",
-  appId: process.env.APP_ID || "1:1098482235546:web:3fc54467864b39a5474723",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
