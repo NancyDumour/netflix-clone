508d74ef6b4df155d275e1bc625500e6bc9afc64
Nancy Dumour <nancydumour96@gmail.com>
2023-01-28 19:45:17 +0200

    Creating Movie and Movies Slider components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import Main from "../components/Main";
+import MoviesSlider from "../components/MoviesSlider";
+import requests from "../Requests";
 const Home = () => {
   return (
     <>
       <Main />
+      <MoviesSlider
+        rowID="1"
+        title="Upcoming"
+        fetchUrl={requests.requestUpcoming}
+      />
+      <MoviesSlider
+        rowID="2"
+        title="Popular"
+        fetchUrl={requests.requestPopular}
+      />
+      <MoviesSlider
+        rowID="3"
+        title="Trending"
+        fetchUrl={requests.requestTrending}
+      />
+      <MoviesSlider
+        rowID="4"
+        title="Top Rated"
+        fetchUrl={requests.requestTopRated}
+      />
+      <MoviesSlider
+        rowID="5"
+        title="Horror"
+        fetchUrl={requests.requestHorror}
+      />
     </>
   );
 };
