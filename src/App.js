6b92f4c6f7a96bb07db3a94e598a972eea1348eb
Nancy Dumour <nancydumour96@gmail.com>
2023-01-28 18:01:36 +0200

    Creating Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div className="App">app</div>;
-}
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
 
 export default App;
